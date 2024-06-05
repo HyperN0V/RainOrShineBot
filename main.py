f27226225e38fe2d4eda7b93d749c84f555708ed
HyperN0V <96358403+HyperN0V@users.noreply.github.com>
2024-06-04 22:36:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -38,35 +38,5 @@ async def weather(ctx: commands.Context, *, city):
     embed.set_thumbnail(url=image_url)
 
     await ctx.send(embed=embed)
-    
-"""Work In Progress
-@bot.command()
-async def forecast(ctx: commands.Context, *, city, days):
-  days = 3
-  url = "http://api.weatherapi.com/v1/forecast.json"
-  par = {"key": os.environ['WEATHER_API_KEY'], "q": city, "days": days}
-
-  async with aiohttp.ClientSession() as session, session.get(url, params=par) as res:
-    data = await res.json()
-    
-    location = data["location"]["name"]
-    temp_cel_max = data["day"]["maxtemp_c"]
-    temp_cel_min = data["day"]["mintemp_c"]
-    temp_far_max = data["day"]["maxtemp_f"]
-    temp_far_min = data["day"]["mintemp_f"]
-    humidity = data["humidity"]
-    image_url = "http:" + data["day"]["condition"]["icon"]
-
-    embed = nextcord.Embed(
-      title=f"{location}'s Forecasted Weather:",
-      description=f"The 3 day forecast for {location} is:",
-      color=nextcord.Color.random())
-    embed.add_field(name="Max Temperature", value=f"{temp_cel_max}°C | {temp_far_max}°F")
-    embed.add_field(name="Min Temperature", value=f"{temp_cel_min}°C | {temp_far_min}°F")
-    embed.add_field(name="Humidity", value=f"{humidity}%")
-    embed.set_thumbnail(url=image_url)
-
-    await ctx.send(embed=embed)
-"""
 
 bot.run(os.environ['TOKEN'])
